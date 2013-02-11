92df79317dccfcafa927bfd6dea9f60193329de8
David Soergel <dev@davidsoergel.com>
2013-02-11 14:17:09 -0500

    dependencies fix

--- clone-dependencies.sh
+++ clone-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/zsh-users/zsh-history-substring-search.git
-git clone https://github.com/rupa/z.git
+git clone git://github.com/zsh-users/zsh-history-substring-search.git
+git clone git://github.com/rupa/z.git
